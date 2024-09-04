a3b090d2d0af7158a474cc2c5d005c1eff65c2b0
Alex Tran <tranduclong092@gmail.com>
2024-09-04 15:05:24 +0300

    Feat: display total cart price with array reduce

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -4,17 +4,24 @@ import ProductCard from "./ProductCard";
 
 function Cart() {
   const cart = useAppSelector(state => state.products.cart);
-  // console.log(cart);
+  // console.log(cart[0].price);
 
   return (
     <Container>
       <h1>Cart</h1>
       {cart.length > 0
-        ? <Container style={{ display: "grid", gridTemplateColumns: "1fr 1fr 1fr 1fr", gap: '2rem' }}>
-          {cart.map(product => (
-            <ProductCard product={product} key={product.id} />
-          ))}
-        </Container>
+        ? <>
+          <Container style={{ display: "grid", gridTemplateColumns: "1fr 1fr 1fr 1fr", gap: '2rem' }}>
+            {cart.map(product => (
+              <ProductCard product={product} key={product.id} />
+            ))}
+          </Container>
+          <Container>
+            <h2>Total:
+              {cart.reduce((total, product) => total + product.price, 0)}
+            </h2>
+          </Container>
+        </>
         : <Container>Cart is empty</Container>
       }
     </Container>
